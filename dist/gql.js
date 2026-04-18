cced8bf061108c4d1c237577df807d54d5c929e3
DonRicoy <noe.ricoy@hotmail.com>
2026-04-17 21:00:48 -0600

    fix: delete folder dist

--- dist/gql.js
+++
@@ -1,8 +0,0 @@
-export const gql = (strings, ...values) => {
-  const fullQuery = strings.reduce((acc, str, i) => {
-    const value = values[i];
-    const parsedValue = value != null ? String(value) : '';
-    return acc + str + parsedValue;
-  }, '');
-  return fullQuery.replace(/\s+/g, ' ').trim();
-};
--- dist/index.js
+++
@@ -1,9 +0,0 @@
-export { Okalit, html, signal, computed, effect, batch } from './Okalit.js';
-export { defineElement } from './define-element.js';
-export { defineChannel, getChannel } from './channel.js';
-export { Router, navigate } from './router.js';
-export { AppMixin, ModuleMixin, PageMixin } from './mixins.js';
-export { t, getI18n } from './i18n.js';
-export { OkalitService, OkalitGraphqlService, RequestControl, service, inject } from './service.js';
-export { OIdle, OWhen, OViewport } from './performance.js';
-export { gql } from './gql.js';
